233c34e70761a8ed50e34dc4c0180ac05a6297dd
Alex Babakhanov <babakhanov1@gmail.com>
2018-05-23 21:17:50 +0300

    Update perl.pl

--- source/perl.pl
+++ source/perl.pl
@@ -1,35 +1,34 @@
 #!/usr/bin/perl
 
 sub shellsSort {
-    my ($arr) = @_;
     my $n     = $#$arr + 1;
     my $gap   = int($n/2);
     my $i     = 0;
     my $j     = 0;
     my $temp  = undef;
-    
+
     while ($gap > 0) {
         $i = $gap;
         while ($i < $n) {
-            $temp = @$arr[$i];
+            $temp = $arr[$i];
             $j = $i;
-            while ($j >= $gap && @$arr[$j - $gap] > $temp) {
-                @$arr[$j] = @$arr[$j - $gap];
+            while ($j >= $gap && $arr[$j - $gap] > $temp) {
+                $arr[$j] = $arr[$j - $gap];
                 $j = $j - $gap;
             }
-            @$arr[$j] = $temp;
+            $arr[$j] = $temp;
             $i = $i + 1;
         }
         $gap = int($gap/2);
     }
 }
 
-my @a = (9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+our @arr = (9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
 my $c = 0;
 
 while ($c < 10000000) {
     shellsSort(\@a);
-    @a = (9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+    @arr = (9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
     $c = $c + 1;
 }
 
