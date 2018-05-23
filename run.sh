323c65cb5e25ce8f9bb1f8317736f1d1a648ecbc
Oleh Hudeichuk <emptystamp@gmail.com>
2018-05-23 19:22:14 +0300

    refactoring

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-# ./engine.pl ./perl.pl 
-./engine.pl 'node ./node.js'
-# ./engine.pl 'ruby ./ruby.rb'
-# ./engine.pl './swift.swift'
-./engine.pl './swift'
-# ./engine.pl ./a.out
+./engine.pl ./source/perl.pl 
+./engine.pl 'node ./source/node.js'
+./engine.pl 'ruby ./source/ruby.rb'
+# ./engine.pl './source/swift.swift'
+./engine.pl './compiled/swift'
+# ./engine.pl ./compiled/a.out
