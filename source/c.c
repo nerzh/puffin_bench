e61f192812989e816dbd607cc04001ad89fc8ee4
Dimitri <dnkorbut@yandex.ru>
2018-05-27 22:24:05 +0300

    dima first c

--- source/c.c
+++ source/c.c
@@ -6,11 +6,34 @@
   return ( *(int*)a - *(int*)b );
 }
 
+void shellsSort(int *arr) {
+		int n 		= 10;
+		int gap		= n / 2;
+		int i			= 0;
+		int j			= 0;
+		int temp	= 0;
+		
+		while(gap > 0) {
+				i = gap;
+				while(i < n) {
+						temp = arr[i];
+						j = i;
+						while(j >= gap && arr[j - gap] > temp) {
+								arr[j] = arr[j - gap];
+								j = j - gap;
+						}
+						arr[j] = temp;
+						i = i + 1;
+				}
+				gap = (int)(gap / 2);
+		}
+}
+
 int main() {
   int arr[10];
-  int c;
+  int c = 0;
 
-  for(c = 0; c < 10000000; c++) {
+  while(c < 10000000) {
     arr[0] = 9;
     arr[1] = 8;
     arr[2] = 7;
@@ -21,9 +44,10 @@ int main() {
     arr[7] = 2;
     arr[8] = 1;
     arr[9] = 0;
-    qsort(arr, 10, sizeof(int), compare);
-
+		c++;
+		shellsSort(arr);
+    //qsort(arr, 10, sizeof(int), compare);
   }
 
-  printf("c 123\n");
+  printf("c %d %d %d %d %d %d %d %d %d %d\n", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9]);
 }
