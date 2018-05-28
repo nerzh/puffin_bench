d7b9fc6acf65f0f126eb62be4d5e03ca0b59b42f
Oleh Hudeichuk <emptystamp@gmail.com>
2018-05-28 03:55:51 +0300

    fix run

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+bash -lc 'mkdir ./compiled' 2>/dev/null
+
 exec_bench() {
   if [ `uname -s` = Linux ]; then
 		exec 3>&1 4>&2
@@ -30,13 +32,15 @@ exec_bench './source/node.js'
 # exec_bench './source/python2.py'
 # exec_bench './source/kotlin.kts'
 # exec_bench './source/elixir.ex'
+
 swiftc -O ./source/swift.swift -o ./compiled/swift
-# swiftc -O ./source/swift2.swift -o ./compiled/swift2
 exec_bench './compiled/swift'
+
+# swiftc -O ./source/swift2.swift -o ./compiled/swift2
 # exec_bench './compiled/swift2'
-go build -o ./compiled/go ./source/go.go
-exec_bench './compiled/go'
+
+# go build -o ./compiled/go ./source/go.go
+# exec_bench './compiled/go'
 
 gcc -O2 -o ./compiled/c.exe ./source/c.c
 exec_bench "./compiled/c.exe"
-# echo "eto Teee -> $T"
