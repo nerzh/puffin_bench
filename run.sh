8a7f226fa17c88ce5678a1e0d21ebd0886b9ea16
Oleh Hudeichuk <emptystamp@gmail.com>
2018-09-25 23:17:10 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -81,7 +81,7 @@ exec_bench './compiled/swift2'
 exec_bench './source/3-writeToFile/node3.js'
 
 # RUBY
-exec_bench './source/3-writeToFile/ruby3.js'
+exec_bench './source/3-writeToFile/ruby3.rb'
 
 # Swift
 swiftc -O -whole-module-optimization ./source/3-writeToFile/swift3.swift -o ./compiled/swift3
